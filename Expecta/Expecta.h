ea770ddc2fc8f092d5dd4ada00658850a91a6624
David Farber and Todd Gihring <pair+df+tg@pivotallabs.com>
2015-02-26 11:45:53 -0700

    Umbrella header must import other headers

--- Expecta/Expecta.h
+++ Expecta/Expecta.h
@@ -9,6 +9,12 @@ FOUNDATION_EXPORT const unsigned char ExpectaVersionString[];
 #import <Expecta/ExpectaObject.h>
 #import <Expecta/ExpectaSupport.h>
 #import <Expecta/EXPMatchers.h>
+#import <Expecta/EXPMatcherHelpers.h>
+#import <Expecta/EXPDoubleTuple.h>
+#import <Expecta/EXPFloatTuple.h>
+#import <Expecta/EXPUnsupportedObject.h>
+#import <Expecta/NSObject+Expecta.h>
+#import <Expecta/NSValue+Expecta.h>
 
 // Enable shorthand by default
 #define expect(...) EXP_expect((__VA_ARGS__))
